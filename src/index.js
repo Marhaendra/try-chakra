927bd180dcbee57a14a0e5a8c31ef1848abfb95a
Marhaendra <marhaendra44@gmail.com>
2021-06-07 18:08:22 +0700

    first push

--- src/index.js
+++ src/index.js
@@ -3,13 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from "@chakra-ui/react"
+import fonts from './assets/font/font';
+import { Global } from "@emotion/react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+        <Global styles={fonts} />
+      </ChakraProvider>
+    </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
